26b6a2a937573cb15b8f993ba4808e4f1f4dead2
Corey Glidden <gnonymuss@gmail.com>
2023-08-31 19:43:57 -0400

    pre 15

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -3,6 +3,7 @@ import { AccountService } from '../_services/account.service';
 import { Observable } from 'rxjs';
 import { User } from '../-models/user';
 import { of } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -13,22 +14,21 @@ export class NavComponent implements OnInit {
   model: any = {};
 
 
-  constructor(public accountService: AccountService) { }
+  constructor(public accountService: AccountService, private router: Router) { }
 
   ngOnInit(): void {
   }  
 
   login() {
     this.accountService.login(this.model).subscribe({
-      next: response => {
-        console.log(response);
-      },
+      next: _ => this.router.navigateByUrl('/members'),  
       error: error => console.log(error)
     })
   }
 
   logout()  {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 
 }
